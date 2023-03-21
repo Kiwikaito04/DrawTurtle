fcce03da94b82a8713fe962b3b18cbb0af2343af
Kiwikaito <nguyenphamdieu41@gmail.com>
2023-03-21 09:23:06 +0700

    change color and size

--- DrawTurtle/DrawTurtle.py
+++ DrawTurtle/DrawTurtle.py
@@ -7,14 +7,14 @@ t=turtle.Turtle()
 t.pensize(5)
 
 #set color
-t.pencolor("red")
-t.fillcolor("blue")
+t.pencolor("blue")
+t.fillcolor("red")
 
 #enable fill color
 t.begin_fill()
 
 #draw circle
-t.circle(100)
+t.circle(150)
 
 #disable fill color
 t.end_fill()
