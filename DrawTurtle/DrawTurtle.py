1f98594bb6beda8411dced01ae20be3b048eaaa7
Kiwikaito <nguyenphamdieu41@gmail.com>
2023-03-21 09:22:00 +0700

    move turtle

--- DrawTurtle/DrawTurtle.py
+++ DrawTurtle/DrawTurtle.py
@@ -3,18 +3,23 @@ import turtle
 #set object t for turtle
 t=turtle.Turtle()
 
+#move the turtle
+t.pencolor("white")
+t.fillcolor("white")
+t.goto(-40,120)
+
 #resize pen
 t.pensize(5)
 
 #set color
-t.pencolor("red")
-t.fillcolor("blue")
+t.pencolor("blue")
+t.fillcolor("red")
 
 #enable fill color
 t.begin_fill()
 
 #draw circle
-t.circle(100)
+t.circle(150)
 
 #disable fill color
 t.end_fill()
